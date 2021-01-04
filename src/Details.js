8872da4dfeb3f3df44b3c5a56e777073447805fc
Karthik Lakshminarayanan <karthiklns83@gmail.com>
2021-01-03 21:56:31 -0500

    overall changes

--- src/Details.js
+++ src/Details.js
@@ -3,79 +3,132 @@ import axios from 'axios';
 import './styles/components/layout.css';
 import * as ReactBootStrap from 'react-bootstrap';
 
-class LayoutSection extends React.Component{
-    constructor(props){
-        super(props);        
+class LayoutSection extends React.Component {
+    constructor(props) {
+        super(props);
         this.calcLayoutSection = this.calcLayoutSection.bind(this);
+        this.sendFormData = this.sendFormData.bind(this);
+        this.callApi = this.callApi.bind(this);
         this.state = {
-            userName:"",
-            userEmail:"",
-            image:"",
-            objectCount:0,
-            objectsList:"",
-            loading:0,
-            error: undefined,
+            userName: "",
+            userEmail: "",
+            image: "",
+            objectCount: 0,
+            objectsList: "",
+            loading: 0,
+            error: undefined
         };
-      }
+        this.url = 'https://dkkmcz6a8g.execute-api.us-east-1.amazonaws.com/dev/upload-to-s3?username=' + this.state.userEmail;
+        this.emailbodyarr = [];
+    }
 
-      onChange(e){
-          let files = e.target.files;
-          console.log('File Details:',files);
-          let reader = new FileReader();
-          reader.readAsDataURL(files[0]);
+    callApi = async (reqBody, reqHeader) => {
+        try {
+            const data = await axios.post(this.url, reqBody, { headers: reqHeader }).then(res => {
+                return res.data.body;
+            })
+            // console.log("returned data is " + JSON.stringify(data));
+            return data
+        } catch (e) {
+            console.log('Error in get labels :' + e);
+        }
+    }
+    onChange(e) {
+        let files = e.target.files;
+        console.log('File Details:', files);
+        let reader = new FileReader();
+        reader.readAsDataURL(files[0]);
 
-          reader.onload = (e) =>{
+        reader.onload = (e) => {
             //console.log('Uploaded Image',e.target.result);
             let imageVal = e.target.result.split(',');
-              this.setState(() => ({ image: imageVal[1]}));
-            }
-      }
-
-        onChangeName(e) {
-            this.setState({ userName: e.target.value });
+            this.setState(() => ({ image: imageVal[1] }));
         }
-
-        onChangeEmail(e) {
-            this.setState({ userEmail: e.target.value });
-        }
-      
-    calcLayoutSection(e){
-      e.preventDefault();
-      const name = (e.target.elements.name.value.trim());
-      const email = (e.target.elements.email.value.trim());
-      this.setState(() => ({ userName: name, userEmail: email, loading:1 }));
-      console.log('Inside calcLayoutSection');
-      let reqBody = {
-          "name":this.state.userName,
-          "email":this.state.userEmail,
-          "image":this.state.image
-        }
-      let reqHeader = {
-        //"Access-Control-Allow-Origin": "*",
-        //"Access-Control-Request-Headers" : "Origin, Content-Type",
-        //"Access-Control-Request-Methods": "OPTIONS,POST",
-        "Content-Type": "application/json"
-      }
-      let url = 'https://dkkmcz6a8g.execute-api.us-east-1.amazonaws.com/dev/upload-to-s3?username='+this.state.userEmail
-
-      const getLabels = async () => {
-          try{
-              const data = await axios.post(url, reqBody, {headers:reqHeader}).then(res => {
-                    console.log('getLabels Response :'+ JSON.stringify(res.data.body))
-                    this.setState(() => ({objectsList:res.data.body, loading:2}));
-                    return res.data.body;
-              })
-              return data
-          } catch(e){
-              console.log('Error in get labels :'+e);
-          }
-      }
-      getLabels();
     }
-    
-    render(){
+
+    onChangeName(e) {
+        this.setState({ userName: e.target.value });
+    }
+
+    onChangeEmail(e) {
+        this.setState({ userEmail: e.target.value });
+    }
+
+    calcLayoutSection(e) {
+        e.preventDefault();
+        const name = (e.target.elements.name.value.trim());
+        const email = (e.target.elements.email.value.trim());
+        this.setState(() => ({ userName: name, userEmail: email, loading: 1 }));
+        console.log('Inside calcLayoutSection');
+        let reqBody = {
+            "name": this.state.userName,
+            "email": this.state.userEmail,
+            "image": this.state.image
+        }
+        let reqHeader = {
+            //"Access-Control-Allow-Origin": "*",
+            //"Access-Control-Request-Headers" : "Origin, Content-Type",
+            //"Access-Control-Request-Methods": "OPTIONS,POST",
+            "Content-Type": "application/json"
+        }
+        // let url = 'https://dkkmcz6a8g.execute-api.us-east-1.amazonaws.com/dev/upload-to-s3?username=' + this.state.userEmail;
+
+        const getLabels = async () => {
+            const apiResp = await this.callApi(reqBody, reqHeader);
+            let emailbodyobj = {};
+            emailbodyobj = apiResp;
+            this.emailbodyarr.push(this.state.userName);
+            this.emailbodyarr.push(this.state.userEmail);
+            emailbodyobj.Labels.map(item => {
+                this.emailbodyarr.push(item.Name);
+                return this.emailbodyarr;
+            })
+            this.setState(() => ({ objectsList: apiResp, loading: 2 }));
+
+        }
+        getLabels();
+    }
+
+    sendFormData(e) {
+        e.preventDefault();
+        console.log(this.emailbodyarr);
+        // let reqBody = {
+        //     "name":this.state.userName,
+        //     "email":this.state.userEmail,
+        //     "list":this.state.list
+        //   }
+
+        let reqBody = {
+            'subject': 'Analysis of the picture',
+            "message": "Dear " +  JSON.stringify(this.state.userName) +  " this is the list of items insured. We will be sending our final analysis to your email address : " + JSON.stringify(this.state.userEmail) + " which you provided. Analysis results are as follows " + this.emailbodyarr
+        }
+
+        let config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        let url = 'https://rk7zodptd5.execute-api.us-east-1.amazonaws.com/Prod?TopicArn=arn:aws:sns:us-east-1:268057325970:ListInfo'
+
+        const sendData = async () => {
+            try {
+                const data = await axios.post(url, reqBody, config).then(res => {
+                    //const data = await axios.post(url, reqBody, {headers:reqHeader}).then(res => {
+                    //   console.log('sendData Response :'+ JSON.stringify(res.data.body))
+                    //   this.setState(() => ({objectsList:res.data.body, loading:3}));
+                    return res.data.body;
+                })
+                return data
+            } catch (e) {
+                console.log('Error in send data :' + e);
+            }
+        }
+        sendData();
+    }
+
+    render() {
         console.log("webClass Render");
-        return(
+        return (
             <div class="container">
                 <form onSubmit={this.calcLayoutSection}>
                     <fieldset>
@@ -85,7 +138,7 @@ class LayoutSection extends React.Component{
                                 <label>Name</label>
                             </div>
                             <div class="col-75">
-                                <input autoComplete='Off' type='text' name='name' placeholder='Enter your name' onChangeName={(e)=>this.onChangeName(e)}/>
+                                <input autoComplete='Off' type='text' name='name' placeholder='Enter your name' onChange={(e) => this.onChangeName(e)} />
                             </div>
                         </div>
 
@@ -94,7 +147,7 @@ class LayoutSection extends React.Component{
                                 <label>Email</label>
                             </div>
                             <div class="col-75">
-                                <input autoComplete='Off' type='text' name='email' placeholder='Enter your email' onChangeEmail={(e)=>this.onChangeEmail(e)}/>
+                                <input autoComplete='Off' type='text' name='email' placeholder='Enter your email' onChange={(e) => this.onChangeEmail(e)} />
                             </div>
                         </div>
 
@@ -103,40 +156,51 @@ class LayoutSection extends React.Component{
                                 <label>Image</label>
                             </div>
                             <div class="col-75">
-                                <input autoComplete='Off' type='file' name='image' onChange={(e)=>this.onChange(e)}/>
+                                <input autoComplete='Off' type='file' name='image' onChange={(e) => this.onChange(e)} />
                             </div>
                         </div>
 
                         <div class="row">
                             <button disabled={((!this.state.userName) && (!this.state.userEmail) && (!this.state.image))}>Identify Objects</button>
-                        </div>          
-                        {this.state.loading === 1 ? 
-                        <div>
-                            <p> Fetching Details....</p>
-                        </div>: 
-                        <p></p>}
-                        
-                        {this.state.loading === 2 ? 
-                        <div class="row">
-                            <div class="col-75">
-                                {/* <label> {JSON.stringify(this.state.objectsList)}</label> */}
+                        </div>
+                        {this.state.loading === 1 ?
+                            <div>
+                                <p> Fetching Details....</p>
+                            </div> :
+                            <p></p>}
+
+                        {this.state.loading === 2 ?
+                            <div class="row">
+                                <div class="col-75">
+                                    {/* <label> {JSON.stringify(this.state.objectsList)}</label> */}
                                     <div>
-                                        <ul>
-                                            <label>List of Names for the picture</label>
-                                            {this.state.objectsList.Labels.map(lst => (
-                                                <li>
-                                                    <label>{lst.Name}</label>
+                                        <ol>
+                                            <label>Objects identified for the uploaded picture:</label>
+                                            {this.state.objectsList.Labels.map((lst, index) => {
+                                                return (<li key={index}>
+                                                    <p>{lst.Name}</p>
                                                 </li>
-                                            ))}
-                                        </ul>
+                                                )
+                                            })}
+                                        </ol>
                                     </div>
-                            </div>
-                        </div>:
-                        <p></p>}
+                                </div>
+                            </div> :
+                            <p></p>}
+
+                        {this.state.loading === 2 ?
+                            <div class="row">
+                                <div class="col-75">
+                                    <div>
+                                        <button onClick={this.sendFormData}>Submit</button>
+                                    </div>
+                                </div>
+                            </div> :
+                            <p></p>}
                     </fieldset>
                 </form>
                 <div>
-                    <ReactBootStrap.Spinner animation="border"/>
+                    <ReactBootStrap.Spinner animation="border" />
                 </div>
             </div>
         )
