100cb84253bfd7bd5ea5f1db9abe108f2dd55653
Karthik Lakshminarayanan <karthiklns83@gmail.com>
2021-01-17 19:23:49 -0500

    show-image branch changes retrofitted

--- src/Details.js
+++ src/Details.js
@@ -242,6 +242,8 @@ class LayoutSection extends React.Component {
                             <div class="row">
                                 <div class="col-75">
                                     <div>
+                                        <img style = {{display: 'flex', width:'100%', height:'20rem' }}
+                                        src={`data:image/png;base64,${this.state.image}`} />
                                         {/* <ol>
                                             <label>Objects identified for the uploaded picture:</label>
                                             {this.state.objectsList.Labels.map((lst, index) => {
